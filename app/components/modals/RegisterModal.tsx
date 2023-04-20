ec8ce33dd0f76ec97aba883a9b60aa6d88ae8915
Nikita <poker6-96@mail.com>
2023-04-20 15:37:55 +0300

    social login (github and google)

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import Heading from "@/app/components/Heading";
 import Input from "@/app/components/inputs/Input";
 import toast from "react-hot-toast";
 import Button from "@/app/components/Button";
+import {signIn} from "next-auth/react";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -77,8 +78,8 @@ const RegisterModal = () => {
     const footerContent = (
         <div className="flex flex-col gap-4 mt-3">
             <hr/>
-            <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}}/>
-            <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}}/>
+            <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => signIn('google')}/>
+            <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => signIn('github')}/>
             <div className="text-neutral-500 text-center mt-4 font-light">
                 <div className=" justify-center flex flex-row items-center gap-2">
                     <div>Already have an account?</div>
